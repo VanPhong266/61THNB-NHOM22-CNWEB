e7fb9009fefbd9c79022715c7d3495c8e99316f2
nguyenquantung <nguyenquantung2001@gmail.com>
2022-01-15 20:40:06 +0700

    comit

--- admin/login_admin.php
+++ admin/login_admin.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="signin.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
 </head>
 <body>
 <header>
@@ -14,7 +14,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container ">
                 <a class="navbar-brand mx-auto order-0" href="index.php">
-                    <img src="img/edmodo.png" alt="" class="ed-logo">
+                    <img src="/assets/img/edmodo.png" alt="" class="ed-logo">
                 </a>
             </div>
             </nav>
